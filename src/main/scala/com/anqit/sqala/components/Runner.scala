9852c0b0ebe7717f45bbe8967efa0b292883c6b0
Unknown <anqit.praqash@gmail.com>
2018-02-02 20:09:56 -0700

    Random restarts

--- src/main/scala/com/anqit/sqala/components/Runner.scala
+++ src/main/scala/com/anqit/sqala/components/Runner.scala
@@ -5,27 +5,36 @@ import scala.util.Random
 class Runner[S <: State, A <: Action](states: Set[S], actions: Set[A], r: Reward[S, A], d: Delta[S, A], start: S, gamma: Double) {
     require(gamma >= 0 && gamma < 1)
 
-    val q = Q(states, actions)
+    var q = Q(states, actions)
 
     var s = start;
 
     def run(): Q[S, A] = {
         var i = 0;
         while(i < 100) {
-            val a = getAction
-            val next = d(s, a)
-            val reward = r(s, a)
+            var j = 0;
+            while(j < 100) {
+                val a = getAction
+                val next = d(s, a)
+                val reward = r(s, a)
 
-            q.update(s, a, reward, gamma, next)
+                q.update(s, a, reward, gamma, next)
 
-            s = next
+                s = next
+                j += 1
+            }
+
             i += 1
+            s = getState
         }
-
         q
     }
 
     def getAction(): A = {
         actions.toList(new Random().nextInt(actions.size))
+    }
+
+    def getState(): S = {
+        states.toList(new Random().nextInt(states.size))
     }
 }
